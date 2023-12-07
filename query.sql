ca514d44194015de41751992b32ffc85c5dfa694
k1s3n <christoffer@rehnmark.se>
2023-12-07 16:38:42 +0100

    hej

--- query.sql
+++ query.sql
@@ -6,3 +6,16 @@ JOIN program  p on i.program_id = p.program_id;
 
 SELECT * FROM branch
 WHERE program_id = 'DAVM'
+
+
+-- se vilka kurser som är obligatoriska för programmet "Tillämpad Matematik och Statistik"
+SELECT p.name AS Program, c.name AS obligatoriska_kurser FROM mandatory_course_program mcp
+JOIN course c ON c.course_id = mcp.course_id
+JOIN program p ON mcp.program_id = p.program_id
+WHERE p.name = 'Tillämpad Matematik och Statistik'
+
+--
+SELECT c.name AS Tillämpad_Matematik_och_Statistik FROM mandatory_course_program mcp
+JOIN course c ON c.course_id = mcp.course_id
+JOIN program p ON mcp.program_id = p.program_id
+WHERE p.name = 'Tillämpad Matematik och Statistik'
